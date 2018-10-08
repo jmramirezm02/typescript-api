5a3d3688b5225318a414172217a199a571ef925a
Jose Miguel R <jmramirezm02@liverpool.com.mx>
2018-10-08 10:56:37 -0500

    Port changed to AWS EBS environment

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express = require("express");
 
 // Our Express APP config
 const app = express();
-app.set("port", process.env.PORT || 5005);
+app.set("port", process.env.PORT || 8080);
 
 // API Endpoints
 app.get('/', (req, res) => {
